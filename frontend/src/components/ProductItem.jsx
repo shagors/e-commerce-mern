09436911e4a1e62169a5e86e54a1b3a1f8079ba8
shagors <shajjadshagor@gmail.com>
2025-02-02 14:31:38 +0600

    make latest products section and design it and make reusable card for this

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
+
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
+      <div className="overflow-hidden">
+        <img
+          src={image[0]}
+          alt={name}
+          className="hover:scale-110 transition ease-in-out"
+        />
+      </div>
+
+      <p className="pt-3 pb-1 text-sm font-semibold">{name}</p>
+      <p className="text-sm font-medium">
+        {currency} {price}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductItem;
