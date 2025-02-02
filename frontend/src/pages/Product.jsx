15ce32c7e5423478ef63a5b7cebace98e796b5ee
shagors <shajjadshagor@gmail.com>
2025-02-02 23:34:52 +0600

    make product page and design it. image and details add not completed yet

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,9 +1,92 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const Product = () => {
   const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState("");
+  const [sizes, setSizes] = useState("");
 
-  return <div>Product</div>;
+  const fetchProductData = async () => {
+    products.map((product) => {
+      if (product._id === productId) {
+        setProductData(product);
+        setImage(product.image[0]);
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div className="flex gap-12 flex-col sm:flex-row">
+        {/* product Image */}
+        <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                key={index}
+                src={item}
+                alt="Image"
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                onClick={() => setImage(item)}
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img src={image} alt="Image" className="w-full h-auto" />
+          </div>
+        </div>
+        {/* product Info */}
+        <div className="flex-1">
+          <h1 className="text-2xl font-medium mt-2">{productData.name}</h1>
+          <div className="flex items-center gap-1 mt-2">
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3.5" />
+            <p className="pl-2">(122)</p>
+          </div>
+          <p className="text-3xl font-medium mt-5">
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p className="">Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  key={index}
+                  className={`border border-gray-100 py-2 px-4 cursor-pointer ${
+                    item === sizes ? "border-orange-500" : ""
+                  }`}
+                  onClick={() => setSizes(item)}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button className="bg-black text-white px-8 py-3 cursor-pointer active:bg-gray-700">
+            ADD TO CART
+          </button>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0">Loading...</div>
+  );
 };
 
 export default Product;
