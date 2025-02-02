94f0b697d4c3e7c5cb73d50e97fa0256af8f3824
shagors <shajjadshagor@gmail.com>
2025-02-02 11:26:20 +0600

    make navbar full complete and resposive check. and also add banner section and add contest provider for using all products

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} alt="Logo" className="w-36" />
@@ -24,6 +27,91 @@ const Navbar = () => {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
+
+      <div className="flex items-center gap-6">
+        <img
+          src={assets.search_icon}
+          alt="Search"
+          className="w-5 cursor-pointer"
+        />
+
+        <div className="group relative">
+          <img
+            src={assets.profile_icon}
+            alt="Profile"
+            className="w-5 cursor-pointer"
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} alt="Cart" className="w-5 min-w-5" />
+          <p className="absolute w-4 right-[-5px] bottom-[-5px] text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
+        </Link>
+
+        <img
+          src={assets.menu_icon}
+          alt="Menu"
+          className="w-5 cursor-pointer sm:hidden"
+          onClick={() => setVisible(true)}
+        />
+      </div>
+
+      {/* Sidebar for mobile screen */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img
+              src={assets.dropdown_icon}
+              alt="Dropdown"
+              className="h-4 rotate-180"
+            />
+            <p className="">Back</p>
+          </div>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/"
+            className="flex items-center gap-4 p-3 border-t border-b border-gray-200"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/collection"
+            className="flex items-center gap-4 p-3 border-t border-b border-gray-200"
+          >
+            COLLECTION
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/about"
+            className="flex items-center gap-4 p-3 border-t border-b border-gray-200"
+          >
+            ABOUT
+          </NavLink>
+          <NavLink
+            onClick={() => setVisible(false)}
+            to="/contact"
+            className="flex items-center gap-4 p-3 border-t border-gray-200"
+          >
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };
