92fa5662a6b4bcdbebe6b02e1d1e2dc27330ee5e
shagors <shajjadshagor@gmail.com>
2025-02-05 22:40:11 +0600

    make controller for register and login and test it it's validation. works perfectly and validation works. token generate login and register user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,85 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
+// Route for user login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists" });
+    }
+
+    const ismatch = await bcrypt.compare(password, user.password);
+
+    if (ismatch) {
+      const token = createToken(user._id);
+      return res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// route for user Registration
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //  checking user exist or not
+    const exists = await userModel.findOne({ email });
+
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validating email format & password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // password hashing
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// route for admin login
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDb from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 // API endpoints
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API is working!");
