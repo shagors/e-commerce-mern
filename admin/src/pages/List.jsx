412bf861a4d4f3a190219fd8a70bc1592ad11e68
shagors <shajjadshagor@gmail.com>
2025-02-10 10:15:12 +0600

    product add from admin panel and test it that product store in db and use token for auth

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 const List = () => {
-  return <div>List</div>;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:4000" + "/api/product/lists"
+      );
+
+      if (response?.data?.products) {
+        setList(response?.data?.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return <></>;
 };
 
 export default List;
